aebf6e3f1de2ab70d8785fca99754d8455d1142d
Michelle Liang <smlxm08@gmail.com>
2024-06-11 21:04:23 +1000

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,13 @@ import store from "./store";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import EmployeesPage from "./pages/EmployeePage/EmployeesPage";
 import AddEmployeePage from "./pages/AddEmployeePage/AddEmployeePage";
+import NavBar from "./components/NavBar/NavBar";
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
+      <NavBar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/employees" element={<EmployeesPage />} />
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import styles from "./NavBar.module.scss";
+
+const NavBar: React.FC = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <Link to="/" className={styles.navLink}>
+            Home
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/employees" className={styles.navLink}>
+            View All Employees
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/add" className={styles.navLink}>
+            Add Employee
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar
